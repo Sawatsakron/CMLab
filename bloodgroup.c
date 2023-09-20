97ff08261dc2fa6550029a56d5efc2d0c3b58cfb
Jumponpatha <jumponpat59@gmail.com>
2023-09-20 23:01:42 +0700

    add TimeGreeting

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,7 +5,7 @@ void main()
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("Do you know Dreamo-Demi-Blue?");
     scanf("%s", Name);
     do
     {
@@ -44,5 +44,6 @@ void main()
     printf("\nContinue (YES for Yes)? ");
     scanf("%s", answer);
   } while (strcmp(answer, "YES") == 0);
-  printf("Goodbye\n");
+  printf("Say:");
+  printf("Ambatukammmn");
 }
